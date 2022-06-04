f9d824b6ca3650160314416a14b962026e3fcee0
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-06-04 16:20:23 -0500

    Updated org template

--- tangled/dplyr.R
+++ tangled/dplyr.R
@@ -1,35 +1,97 @@
 library(dplyr)
+library(data.table)
+library(tidyr)
 
 mtcars
 
-mtcarsR <- tibble::rownames_to_column(mtcars, 'car_name')                         
-mtcarsR
+mtcarsR <-
+  mtcars %>%
+  tibble::rownames_to_column('car_name') %>%
+  data.table::data.table()
+mtcarsR %>%
+  head()
 
-dplyr::filter(mtcarsR, cyl>4 & carb==4)
+mtcarsR %>%
+  dplyr::group_by(cyl, carb) %>%
+  dplyr::select(mpg, hp, cyl, carb) %>%
+  dplyr::summarise(meanMPG=mean(mpg, na.rm=TRUE),
+                   meanCYL=mean(hp,  na.rm=TRUE),
+                   .groups = 'drop')  %>%
+  filter(meanMPG > 20) %>%
+  data.table::data.table()
 
-dplyr::slice(mtcarsR, 15:20)
+tmp <- dplyr::group_by(mtcarsR, cyl, carb)
+tmp <- dplyr::select(tmp, mpg, hp, cyl, carb)
+tmp <- dplyr::summarise(tmp,
+                        meanMPG=mean(mpg, na.rm=TRUE),
+                        meanCYL=mean(hp,  na.rm=TRUE),
+                        .groups = 'drop')
+tmp <- dplyr::filter(tmp, meanMPG > 20)
+tmp <- data.table::data.table(tmp)
+tmp
 
-dplyr::distinct(mtcarsR, cyl, carb)
+data.table::data.table(dplyr::filter(dplyr::summarise(dplyr::select(dplyr::group_by(mtcarsR,
+                                                                                    cyl,
+                                                                                    carb),
+                                                                    mpg,
+                                                                    hp,
+                                                                    cyl,
+                                                                    carb),
+                                                      meanMPG=mean(mpg, na.rm=TRUE),
+                                                      meanCYL=mean(hp,  na.rm=TRUE),
+                                                      .groups = 'drop'),
+                                     meanMPG > 20))
 
-dplyr::arrange(mtcarsR, cyl, desc(gear))
+mtcarsR %>%
+  dplyr::filter(cyl>4 & carb==4)
 
-dplyr::select(mtcarsR, cyl:drat)
+mtcarsR %>%
+  dplyr::slice(15:20)
 
-dplyr::select(mtcarsR, displacement=disp, cyl)
+mtcarsR %>%
+  dplyr::distinct(cyl, carb)
 
-dplyr::rename(mtcarsR, displacement=disp, weight=wt)
+mtcarsR %>%
+  dplyr::arrange(cyl, desc(gear))
 
-dplyr::mutate(mtcarsR, mpc=mpg/cyl, impc=1/mpc)
+mtcarsR %>%
+  dplyr::select(cyl:drat) %>%
+  head()
 
-dplyr::transmute(mtcarsR, mpc=mpg/cyl, mpd=mpg/disp)
+mtcarsR %>%
+  dplyr::select(displacement=disp, cyl) %>%
+  head()
 
-dplyr::summarize(mtcars, mean_disp=mean(disp), sd_disp=sd(disp), mean_wt=mean(wt))
+mtcarsR %>%
+  dplyr::rename(displacement=disp, weight=wt) %>%
+  head()
 
-dplyr::summarize(dplyr::group_by(mtcars, cyl), mean_disp_by_cyl=mean(disp), .groups = 'drop')
+mtcarsR %>%
+  dplyr::mutate(mpc=mpg/cyl, impc=1/mpc) %>%
+  head()
 
-dplyr::summarize(dplyr::group_by(mtcars, cyl, gear), mean_disp_by_cyl_and_gear=mean(disp), .groups = 'drop')
+mtcarsR %>%
+  dplyr::transmute(mpc=mpg/cyl, mpd=mpg/disp) %>%
+  head()
 
-dplyr::mutate(dplyr::group_by(mtcars, cyl, gear), mean_disp_by_cyl_and_gear=mean(disp))
+mtcarsR %>%
+  dplyr::summarize(mean_disp=mean(disp), sd_disp=sd(disp), mean_wt=mean(wt)) %>%
+  data.table::data.table()
+
+mtcarsR %>%
+  dplyr::group_by(cyl) %>%
+  dplyr::summarize(mean_disp_by_cyl=mean(disp), .groups = 'drop') %>%
+  data.table::data.table()
+
+mtcarsR %>%
+  dplyr::group_by(cyl, gear) %>%
+  dplyr::summarize(, mean_disp_by_cyl_and_gear=mean(disp), .groups = 'drop') %>%
+  data.table::data.table()
+
+mtcarsR %>%
+  dplyr::group_by(cyl, gear) %>%
+  dplyr::mutate(mean_disp_by_cyl_and_gear=mean(disp)) %>%
+  data.table::data.table()
 
 carSurvey <- data.table::fread(header=T, text='
                car_name, GEARS, word
@@ -46,43 +108,45 @@ carSurvey <- data.table::fread(header=T, text='
    ')
 carSurvey
 
-dplyr::inner_join(mtcarsR, carSurvey, by=c('car_name', 'gear'='GEARS'));
-
-dplyr::left_join(mtcarsR, carSurvey, by=c('car_name', 'gear'='GEARS'));
-
-dplyr::right_join(mtcarsR, carSurvey, by=c('car_name', 'gear'='GEARS'));
-
-dplyr::full_join(mtcarsR, carSurvey, by=c('car_name', 'gear'='GEARS'));
-
-dplyr::semi_join(mtcarsR, carSurvey, by=c('car_name', 'gear'='GEARS'));
-
-dplyr::inner_join(mtcarsR, rename(carSurvey, gear=GEARS, survey_word=word), by=c('car_name', 'gear'));
+mtcarsR %>%
+  dplyr::inner_join(carSurvey, by=c('car_name', 'gear'='GEARS'))
 
 mtcarsR %>%
-  dplyr::group_by(cyl, carb) %>%
-  dplyr::select(mpg, hp, cyl, carb) %>%
-  dplyr::summarise(meanMPG=mean(mpg, na.rm=TRUE),
-                   meanCYL=mean(hp,  na.rm=TRUE),
-                   .groups = 'drop')  %>%
-  filter(meanMPG > 20)
+  dplyr::left_join(carSurvey, by=c('car_name', 'gear'='GEARS'))
 
-tmp <- dplyr::group_by(mtcarsR, cyl, carb)
-tmp <- dplyr::select(tmp, mpg, hp, cyl, carb)
-tmp <- dplyr::summarise(tmp,
-                        meanMPG=mean(mpg, na.rm=TRUE),
-                        meanCYL=mean(hp,  na.rm=TRUE),
-                        .groups = 'drop')
-tmp <- dplyr::filter(tmp, meanMPG > 20)
-tmp
+mtcarsR %>%
+  dplyr::right_join(carSurvey, by=c('car_name', 'gear'='GEARS'));
 
-dplyr::filter(dplyr::summarise(dplyr::select(dplyr::group_by(mtcarsR, 
-                                                             cyl, 
-                                                             carb), 
-                                             mpg, 
-                                             hp, 
-                                             cyl, 
-                                             carb),
-                               meanMPG=mean(mpg, na.rm=TRUE),
-                               meanCYL=mean(hp,  na.rm=TRUE),
-                               .groups = 'drop'), 
-              meanMPG > 20)
+mtcarsR %>%
+  dplyr::full_join(carSurvey, by=c('car_name', 'gear'='GEARS'));
+
+mtcarsR %>%
+  dplyr::semi_join(carSurvey, by=c('car_name', 'gear'='GEARS'));
+
+mtcarsR %>%
+  dplyr::inner_join(rename(carSurvey, gear=GEARS, survey_word=word), by=c('car_name', 'gear'));
+
+longData <-
+  mtcarsR %>%
+  dplyr::select(gear, carb, mpg) %>%
+  dplyr::group_by(gear, carb) %>%
+  dplyr::summarise(mpg=mean(mpg), .groups = 'drop') %>%
+  data.table::data.table()
+longData
+
+wideData <-
+  longData %>%
+  tidyr::pivot_wider(names_from=carb,
+                     names_prefix='carb_',            ### This prepends "carb_" to the column titles created from the  carb column
+                     values_from=mpg) %>%
+  data.table::data.table()
+wideData
+
+wideData %>%
+  tidyr::pivot_longer(cols=2:7,
+                      names_to="carb",
+                      names_prefix='carb_',                     ### Wack the "carb_" we added to the column names.
+                      names_transform = list(carb=as.integer),  #### Previous line gets rid of the "carb_", but left a string.  Convert it to integers.
+                      values_drop_na=TRUE,                      ### Get rid of the NA values we created when we made teh wide dta
+                      values_to='mpg') %>%
+  data.table::data.table()
